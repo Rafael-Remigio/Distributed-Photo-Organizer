a01324405373b1afd293a3ba772cdf3aa5aaa639
rafael <rafael.lixa@ua.pt>
2022-06-12 23:44:28 +0100

    start2

--- daemon.py
+++ daemon.py
@@ -1,1 +1,41 @@
+from email.mime import image
+import sys
+from os import listdir
+from os.path import isfile, join
+import PIL
+import imagehash
+from PIL import Image
+import os
+
+
+def imageHashing(args):
+
+    imageHashes = {}  # name : hash
+
+    if len(args) <= 0:
+        print("INSERT AN IMAGE FOLDER")
+        sys.exit()
+
+
+    files = [f for f in listdir(args[0]) if isfile(join(args[0], f))]
+
+
+    for currentImage in files:
+        hash = imagehash.average_hash(Image.open(args[0] + currentImage))
+        if (hash in imageHashes.values()):
+            print("Image  "+ (args[0] + currentImage)+" is repeated."+ " It was Removed")
+            os.remove(args[0] + currentImage)
+            continue
+        print(currentImage + "   " + hash.__str__())
+        imageHashes[currentImage] = hash
+
+    
+
+
+if __name__ == "__main__":
+    args = sys.argv[1:]
+
+    imageHashing(args)
+     
+     
 
